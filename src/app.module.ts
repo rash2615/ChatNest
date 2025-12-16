9fb70363fb61d2aadd4d22d14fab034e8e95534c
Rashmi <rashmi.bhavsar02@gmail.com>
2025-12-16 21:59:22 +0100

    Configurations des messages

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatModule } from './chat/chat.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
-  imports: [ChatModule],
+  imports: [ChatModule, UsersModule, AuthModule, MessagesModule],
   controllers: [AppController],
   providers: [AppService],
 })
